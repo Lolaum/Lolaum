03722270614e9a28bd98b139b9535c8eb138b1e7
jjy0328 <wjdwndud3907@naver.com>
2026-01-25 21:07:57 +0900

    Fix/Home: 구글 로그인 관련 코드 삭제

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -2,21 +2,9 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
-    }),
-  ],
+  providers: [],
   pages: {
     signIn: "/login",
   },
-  callbacks: {
-    async session({ session, token }) {
-      if (token.sub && session.user) {
-        session.user.id = token.sub;
-      }
-      return session;
-    },
-  },
+  callbacks: {},
 });
