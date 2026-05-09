ad5b367bbc860cf28dc683c8ae014659bcd53e59
jjy0328 <wndud1375@gmail.com>
2026-05-09 10:00:40 +0900

    -DYNAMIC_SERVER_USAGE 에러 수정

--- src/app/progress/page.tsx
+++ src/app/progress/page.tsx
@@ -2,7 +2,7 @@ import Layout from "@/components/Layout/Layout";
 import ProgressContainer from "@/components/Progress/ProgressContainer";
 import { getProgressPageData } from "@/api/progress";
 
-export const revalidate = 300;
+export const dynamic = "force-dynamic";
 
 export default async function ProgressPage() {
   const result = await getProgressPageData();
